c0858a1abcca22b61c9163aa38f3d9d2f051677a
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-20 21:36:46 +0100

    Multiple Choice Block Type
    Using Multiple Choice Block Type, Event Handling & Updating Block Attributes, also block styling

--- index.php
+++ index.php
@@ -20,13 +20,16 @@ class AttentionQuiz {
       }
     
       function adminAssets() {
-        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element'));
+        wp_register_style('quizeditcss', plugin_dir_url(__FILE__) . 'build/index.css');
+        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
         register_block_type('ourplugin/attention-quiz', array(
           'editor_script' => 'ournewblocktype',
+          'editor_style' => 'quizeditcss',
           'render_callback' => array($this, 'theHTML')
         ));
       }
-    
+
+
       function theHTML($attributes) {
         ob_start(); ?>
         <h3>Today the sky is completely <?php echo esc_html($attributes['skyColor']) ?> and the grass is <?php echo esc_html($attributes['grassColor']) ?>!</h3>
--- src/index.js
+++ src/index.js
@@ -1,29 +1,61 @@
-wp.blocks.registerBlockType("ourplugin/attention-quiz", {
-    title: "Attention Quiz",
-    icon: "smiley",
-    category: "common",
-    attributes: {
-      skyColor: {type: "string"},
-      grassColor: {type: "string"}
-    },
-    edit: function (props) {
-      function updateSkyColor(event) {
-        props.setAttributes({skyColor: event.target.value})
-      }
-  
-      function updateGrassColor(event) {
-        props.setAttributes({grassColor: event.target.value})
-      }
-      return (
-        <div>
-        <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
-        <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
-      </div>
-      )
-    },
+import "./index.scss"
+import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from "@wordpress/components"
 
-    save: function (props) {
-      return  null
-    },
-  })
-  
+wp.blocks.registerBlockType("ourplugin/attention-quiz", {
+  title: "Attention Quiz",
+  icon: "smiley",
+  category: "common",
+  attributes: {
+    question: { type: "string" },
+    answers: { type: "array", default: ["red", "blue", "green"] }
+  },
+  edit: EditComponent,
+  save: function (props) {
+    return null
+  }
+})
+
+function EditComponent(props) {
+
+  function updateQuestion(value) {
+    props.setAttributes({ question: value })
+  }
+
+  function deleteAnswer(indexToDelete) {
+    const newAnswers = props.attributes.answers.filter(function (x, index) {
+      return index != indexToDelete
+    })
+    props.setAttributes({ answers: newAnswers })
+  }
+
+  return (
+    <div className="paying-attention-edit-block">
+      <TextControl label="Question:" value={props.attributes.question} onChange={updateQuestion} style={{ fontSize: "20px" }} />
+      <p style={{ fontSize: "13px", margin: "20px 0 8px 0" }}>Answers:</p>
+      {props.attributes.answers.map(function (answer, index) {
+        return (
+          <Flex>
+            <FlexBlock>
+              <TextControl value={answer} onChange={newValue => {
+                const newAnswers = props.attributes.answers.concat([])
+                newAnswers[index] = newValue
+                props.setAttributes({ answers: newAnswers })
+              }} />
+            </FlexBlock>
+            <FlexItem>
+              <Button>
+                <Icon className="mark-as-correct" icon="star-empty" />
+              </Button>
+            </FlexItem>
+            <FlexItem>
+              <Button isLink className="attention-delete" onClick={() => deleteAnswer(index)}>Delete</Button>
+            </FlexItem>
+          </Flex>
+        )
+      })}
+      <Button isPrimary onClick={() => {
+        props.setAttributes({ answers: props.attributes.answers.concat([""]) })
+      }}>Add another answer</Button>
+    </div>
+  )
+}
