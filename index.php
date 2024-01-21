1402b4123da03acf097561921d062a11dcbb49c7
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-21 11:26:21 +0100

    adding package.json

--- index.php
+++ index.php
@@ -20,23 +20,13 @@ class AttentionQuiz {
       }
     
       function adminAssets() {
-        wp_register_style('quizeditcss', plugin_dir_url(__FILE__) . 'build/index.css');
-        wp_register_script('ournewblocktype', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
-        //wp_enqueue_script('attentionFrontend', plugin_dir_url(__FILE__) . 'build/frontend.js', array('wp-element'), '1.0', true);
-        register_block_type('ourplugin/attention-quiz', array(
-          'editor_script' => 'ournewblocktype',
-          'editor_style' => 'quizeditcss',
+        register_block_type(__DIR__, array(
           'render_callback' => array($this, 'theHTML')
         ));
       }
 
 
       function theHTML($attributes) {
-        if (!is_admin()) {
-          wp_enqueue_script('attentionFrontend', plugin_dir_url(__FILE__) . 'build/frontend.js', array('wp-element'));
-          wp_enqueue_style('attentionFrontendStyles', plugin_dir_url(__FILE__) . 'build/frontend.css');
-        }    
-    
         ob_start(); ?>
         <div class="paying-attention-update-me"><pre style="display: none;"><?php echo wp_json_encode($attributes) ?></pre></div>
         <?php return ob_get_clean();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./index.scss";
 import {TextControl, Flex, FlexBlock, FlexItem, Button, Icon, PanelBody, PanelRow, ColorPicker} from "@wordpress/components";
-import {InspectorControls, BlockControls, AlignmentToolbar } from "@wordpress/block-editor";
+import {InspectorControls, BlockControls, AlignmentToolbar, useBlockProps } from "@wordpress/block-editor";
 import {ChromePicker} from "react-color";
 
 
@@ -53,6 +53,10 @@ wp.blocks.registerBlockType("ourplugin/attention-quiz", {
 });
 
 function EditComponent(props) {
+  const BlockProps = useBlockProps({
+    className: "paying-attention-edit-block",
+     style: {backgroundColor: props.attributes.bgColor}
+  });
 
   function updateQuestion(value) {
     props.setAttributes({ question: value });
@@ -74,7 +78,7 @@ function EditComponent(props) {
   }
 
   return (
-    <div className="paying-attention-edit-block" style={{backgroundColor: props.attributes.bgColor}}>
+    <div {...BlockProps}>
           
           <BlockControls>
         <AlignmentToolbar value={props.attributes.theAlignment} onChange={x => props.setAttributes({ theAlignment: x })} />
