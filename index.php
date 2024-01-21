e5d03524316ce2347db1cac5289534a7b058da77
Favouritecoder <favourgabrielcutie08@gmail.com>
2024-01-21 09:07:21 +0100

    Passing Block Data From PHP Into JavaScript / React

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ class AttentionQuiz {
         }    
     
         ob_start(); ?>
-        <div class="paying-attention-update-me"></div>
+        <div class="paying-attention-update-me"><pre style="display: none;"><?php echo wp_json_encode($attributes) ?></pre></div>
         <?php return ob_get_clean();
       }
     }
--- src/frontend.js
+++ src/frontend.js
@@ -5,14 +5,20 @@ import "./frontend.scss"
 const divsToUpdate = document.querySelectorAll(".paying-attention-update-me")
 
 divsToUpdate.forEach(function(div) {
-  ReactDOM.render(<Quiz />, div)
-  div.classList.remove("paying-attention-update-me")
-})
-
-function Quiz() {
-  return (
-    <div className="paying-attention-frontend">
-      Hello from Developer
-    </div>
-  )
-}
+    const data = JSON.parse(div.querySelector("pre").innerHTML)
+    ReactDOM.render(<Quiz {...data} />, div)
+    div.classList.remove("paying-attention-update-me")
+  })
+  
+  function Quiz(props) {
+    return (
+      <div className="paying-attention-frontend">
+        <p>{props.question}</p>
+        <ul>
+          {props.answers.map(function(answer) {
+            return <li>{answer}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
